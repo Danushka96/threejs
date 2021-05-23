8131e7ac5cb997429ac78b38e3be4e8605dda7b7
danushka96 <danushkaherath96@gmail.com>
2021-05-23 13:39:54 +0530

    intial commit

---
+++ main.js
@@ -0,0 +1,89 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+//main elements for render graphics
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+//set screen sized
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+renderer.render(scene, camera);
+
+// Torus
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xFF6347
+})
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+// lighting
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20,20,20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(100, 50);
+scene.add(lightHelper, gridHelper);
+
+// mouse pointer events detector
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// add custom stars to random points
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff
+  })
+
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
+// set background
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+// avatar cube
+const avatarTexture = new THREE.TextureLoader().load('avatar.jpg');
+const boxGeometry = new THREE.BoxGeometry(3, 3 ,3);
+
+const avatar = new THREE.Mesh(
+  boxGeometry,
+  new THREE.MeshBasicMaterial({
+    map: avatarTexture
+  })
+)
+
+avatar.position.set(20, 10, 10);
+
+scene.add(avatar);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
